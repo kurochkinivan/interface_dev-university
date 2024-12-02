848b0928a17c280acadb09fa83e2c856a742757c
Kur04ka <vanya.kurochkin.23@mail.ru>
2024-12-02 14:44:05 +0700

    add publisher/subscriber to winform

--- winform/cmd/app/main.go
+++ winform/cmd/app/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2/app"
 	"github.com/kurochkinivan/winform/internal/constants"
-	"github.com/kurochkinivan/winform/internal/menu"
+	"github.com/kurochkinivan/winform/internal/model"
+	"github.com/kurochkinivan/winform/internal/view"
 )
 
 func main() {
@@ -11,10 +14,20 @@ func main() {
 	w := a.NewWindow("Мое приложение")
 	w.Resize(constants.DefaultSize)
 
-	menu := menu.CreateMenu(&a)
+	menu := view.CreateMenu(&a)
 	w.SetMainMenu(menu)
 
 	w.Show()
 	w.SetMaster()
 	a.Run()
+}
+
+type ExampleSubscriber struct {
+	ID       string
+	settings *model.Settings
+}
+
+// Update - метод, вызываемый при изменении в Settings.
+func (es *ExampleSubscriber) Update() {
+	fmt.Printf("Subscriber %s notified: PathToStatistics is now '%s'\n", es.ID, es.settings.PathToStatistics)
 }
